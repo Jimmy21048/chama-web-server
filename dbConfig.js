8da9fd9edfdd22b3a0501eaf1e4e1edd022d1357
Jimmy Rubia <rubiajimmy3@gmail.com>
2025-03-02 23:45:48 +0300

    implements useExists and addUser routes

--- dbConfig.js
+++ dbConfig.js
@@ -1,8 +1,11 @@
 const mysql = require('mysql')
+require('dotenv').config()
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'testdb',
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
 })
+
+module.exports = connection
--- index.js
+++ index.js
@@ -1,14 +1,49 @@
 const express = require('express')
 const cors = require('cors')
+const connection = require('./dbConfig')
+require('dotenv').config()
 
 const app = express()
 app.use(cors())
 app.use(express.json());
 
+app.post('/addUser', (req, res) => {
+    const query = "INSERT INTO chama (username) VALUES (?)";
+    const values = [req.body.username]
 
+    connection.query(query, values, (err) => {
+        if(err) return console.log(err);
 
-app.listen(3000, (err) => {
+        return res.json({added: true})
+    })
+})
+
+app.get('/getUsers', (req, res) => {
+    const query = "SELECT * FROM chama";
+
+    connection.query(query, (err, result) => {
+        if(err) return console.log(err);
+
+        console.log(result)
+    })
+})
+app.post('/userExists', (req, res) => {
+    const query = "SELECT username FROM chama WHERE username = ?";
+    const values = [req.body.username]
+
+    connection.query(query, values, (err, result) => {
+        if(err) return console.log(err);
+
+        if(result.length > 0) return res.json({exists: true});
+        return res.json({exists: false})
+    })
+})
+
+connection.connect((err) => {
     if(err) return console.log(err);
-
-    console.log("Server up and running")
+    app.listen(3000, (err) => {
+        if(err) return console.log(err);
+    
+        console.log("Server up and running")
+    })
 })
