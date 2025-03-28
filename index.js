05d92279e73fbc5ff7224a867758b995583afcf1
Jimmy Rubia <rubiajimmy3@gmail.com>
2025-03-28 16:50:06 +0300

    initialize router for updating rounds

--- index.js
+++ index.js
@@ -86,6 +86,10 @@ app.post('/updateAmount', (req, res) => {
     })
 })
 
+app.post('/updateRounds', (req, res) => {
+    
+})
+
 connection.connect((err) => {
     if(err) return console.log(err);
     app.listen(3000, (err) => {
